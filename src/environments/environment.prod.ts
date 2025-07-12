f459a85d0b24822e730c5eee6490e51163d9d79a
ismael kimutai <ismaelkim2@gmail.com>
2025-07-12 18:52:00 +0300

    Update Angular source code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
       production: true,
-      apiUrl: 'https://updated-backend-springboot-apis-production.up.railway.app/api',
+      apiUrl: 'https://updated-backend-springboot-apis-production.up.railway.app',
       cloudinaryCloudName: 'Ismael',  
       cloudinaryUrl: 'https://api.cloudinary.com/v1_1/dg70z4hug/upload',
       cloudinaryPreset: 'custom_upload_preset',
